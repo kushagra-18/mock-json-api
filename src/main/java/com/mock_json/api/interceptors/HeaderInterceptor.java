90fcb5e6659071be6bede11fa1544efc892cb860
kushagra-18 <46473446+kushagra-18@users.noreply.github.com>
2024-10-16 23:30:11 +0530

    interceptor

--- src/main/java/com/mock_json/api/interceptors/HeaderInterceptor.java
+++ src/main/java/com/mock_json/api/interceptors/HeaderInterceptor.java
@@ -22,11 +22,11 @@ public class HeaderInterceptor implements HandlerInterceptor {
             if (handlerMethod.getMethod().isAnnotationPresent(HeaderIntercepted.class) ||
                 handlerMethod.getBeanType().isAnnotationPresent(HeaderIntercepted.class)) {
 
-                // String teamHeader = request.getHeader("X-header-team");
-                // String projectHeader = request.getHeader("X-header-project");
+                String teamHeader = request.getHeader("X-header-team");
+                String projectHeader = request.getHeader("X-header-project");
 
-                String teamHeader = "free";
-                String projectHeader = "kush";
+                // String teamHeader = "free";
+                // String projectHeader = "kush";
 
                 if (teamHeader == null || projectHeader == null) {
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
