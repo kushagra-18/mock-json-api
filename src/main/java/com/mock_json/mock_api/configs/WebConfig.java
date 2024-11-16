58092d57ba1c7112423ea782f3a76b524570e154
kushagra-18 <46473446+kushagra-18@users.noreply.github.com>
2024-11-17 00:57:13 +0530

    cors

--- src/main/java/com/mock_json/mock_api/configs/WebConfig.java
+++ src/main/java/com/mock_json/mock_api/configs/WebConfig.java
@@ -8,7 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
    @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")  // Apply to all endpoints
+                .allowedOrigins("*")  // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow all HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(false);  // Allow credentials if necessary
     }
  
 }
